a4016ef578d95e9409677b0fbf82b8be090b71ee
Humam Alhusaini <humam.alhusaini@proton.me>
2026-08-13 18:07:33 -0500

    removed hacky repl solution to file parsing

--- bin/main.ml
+++ bin/main.ml
@@ -6,13 +6,18 @@ open In_channel
 let rec repl ctx =
   print_string ">>> ";
   let txt = read_line () in
-  if String.starts_with ~prefix:"file " txt then 
-    let chan = String.sub txt 5 (String.length txt - 5) |> open_bin in
-      read (input_all chan) ctx |> repl
-  else 
   match txt with
   | "exit" -> print_endline "Goodbye!"
   | "pctx" -> print_endline (fmap ctx); repl ctx
   | _ -> read txt ctx |> repl
 
-let () = repl Empty;;
+let () = 
+  (*If there is a file as an argument, parse the file*)
+  if Array.length Sys.argv <> 1 then
+    let str = Array.get Sys.argv 1 |> open_bin |> input_all in 
+    let _ = read str Empty in () 
+  else
+    (*Else give a repl*)
+    let _ = print_int (Array.length Sys.argv) in
+  repl Empty
+;;
